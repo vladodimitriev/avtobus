95a410696db00fc81ddd36bd2e5d182cabfce94b
vladodimitriev <vlado.dimitriev@gmail.com>
2017-11-26 19:54:26 +0100

    added cyrillic names in admin page

--- src/main/java/mk/mladen/avtobusi/pages/LoginPage.java
+++ src/main/java/mk/mladen/avtobusi/pages/LoginPage.java
@@ -1,18 +1,11 @@
 package mk.mladen.avtobusi.pages;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.wicket.MarkupContainer;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.PasswordTextField;
 import org.apache.wicket.markup.html.form.TextField;
-import org.apache.wicket.markup.html.image.Image;
-import org.apache.wicket.markup.html.link.Link;
-import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.request.resource.ContextRelativeResourceReference;
-import org.apache.wicket.request.resource.ResourceReference;
 
 public class LoginPage extends BasePage {
 
@@ -25,15 +18,17 @@ public class LoginPage extends BasePage {
 
         Model<String> model2 = new Model<String>();
         PasswordTextField ptf = new PasswordTextField("password", model2);
-        Form form = new Form("loginForm") {
-            @Override
+        Form<String> form = new Form<String>("loginForm") {
+            private static final long serialVersionUID = 1L;
+			@Override
             protected void onSubmit() {
                 String username = model1.getObject();
                 String password = model2.getObject();
                 if(StringUtils.isNotBlank(username) && StringUtils.isNotBlank(password)) {
                     if(username.equals(password)) {
-                        //setResponsePage(AdminPage.class, getParams("EN"));
-                        setResponsePage(AdminHomePage.class, getParams("EN"));
+                    	PageParameters params = new PageParameters();
+                        params.add("lang", lang);
+                        setResponsePage(AdminHomePage.class, params);
                     }
                 }
             }
@@ -42,15 +37,6 @@ public class LoginPage extends BasePage {
         form.add(ptf);
 
         add(form);
-    }
-
-    private PageParameters getParams(String language) {
-        PageParameters params = new PageParameters();
-        if(language != null) {
-            params.add("lang", language);
-        }
-
-        return params;
     }
 
     @Override
